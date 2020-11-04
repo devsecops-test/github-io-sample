0e34913c94918389921033904f9c8bd603086499
Rahul Gunasekaran <71350204+rahulguna10@users.noreply.github.com>
2020-11-04 11:34:08 -0500

    Update TransferDaoImpl.java

--- src/main/java/com/example/dao/TransferDaoImpl.java
+++ src/main/java/com/example/dao/TransferDaoImpl.java
@@ -12,8 +12,24 @@ public class TransferDaoImpl implements TransferDao {
 	private JdbcTemplate jdbcTemplate;
 
 	@Override
-	public void insertTransfer(final Transfer transfer) {
+	public void insertTransfer(final Transfer transfer, final String username) {
+		
+		String str = "select * from account where username='" + username + "'";
 
+		RowMapper<Account> rowMapper = new RowMapper<Account>() {
+			@Override
+			public Account mapRow(final ResultSet paramResultSet, final int paramInt) throws java.sql.SQLException {
+				Account localAccount = new Account();
+				localAccount.setUsername(paramResultSet.getString("username"));
+				localAccount.setName(paramResultSet.getString("name"));
+				localAccount.setSurname(paramResultSet.getString("surname"));
+				localAccount.setPassword(paramResultSet.getString("password"));
+				return localAccount;
+			}
+		};
+
+		return jdbcTemplate.query(str, rowMapper);
+		
 		String sql = "INSERT INTO transfer "
 				+ "(fromAccount, toAccount, description, amount, fee, username, date) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
